56f96dec0436c9c3ebcabd31f495c5ad7868bbf3
miketvo <ikxplicitie@gmail.com>
2021-04-23 00:10:56 +0700

    Successfully implemented infinite card gallery scrolling

--- public/js/cards.js
+++ public/js/cards.js
@@ -1,91 +1,120 @@
-/**
- * Gallery auto scrolling
- */
-
+// DOM Access
 const GALLERIES = document.querySelectorAll(".card-gallery");
 const GALLERIES_LEFT_BTTNS = document.querySelectorAll(".card-gallery-left-bttn");
 const GALLERIES_RIGHT_BTTNS = document.querySelectorAll(".card-gallery-right-bttn");
 const GALLERIES_CONTENTS = document.querySelectorAll(".card-gallery-content");
+
+
+// Gallery auto infinite scrolling
 const DEFAULT_SCROLL_SPEED = 0.05;
-const FAST_SCROLL_SPEED = 50;
+const MAX_SCROLL_SPEED = 100;
 const DEFAULT_SCROLL_DIR = 1;
 
-
-if (GALLERIES.length === GALLERIES_CONTENTS.length &&
-    GALLERIES_LEFT_BTTNS.length === GALLERIES.length &&
-    GALLERIES_RIGHT_BTTNS.length === GALLERIES.length
+if (GALLERIES.length !== GALLERIES_CONTENTS.length ||
+    GALLERIES_LEFT_BTTNS.length !== GALLERIES.length ||
+    GALLERIES_RIGHT_BTTNS.length !== GALLERIES.length
 ) {
-    let scrollDir = DEFAULT_SCROLL_DIR
+    console.log("ERROR: Card gallery elements does not match each other");
+} else {
+    // Duplicate the contents of card gallery for infinite scrolling effect, only if that card gallery overflows
+    let scrollables = [];
+    for (let index = 0; index < GALLERIES_CONTENTS.length; index++) {
+        if (GALLERIES_CONTENTS[index].clientWidth < GALLERIES_CONTENTS[index].scrollWidth) {
+            scrollables.push(true);
+            GALLERIES_CONTENTS[index].childNodes.forEach(function (item) {
+                let cloneItem = item.cloneNode(true);
+                GALLERIES_CONTENTS[index].appendChild(cloneItem);
+            });
+        } else {
+            scrollables.push(false);
+            GALLERIES_LEFT_BTTNS[index].setAttribute("style", "display: none;");
+            GALLERIES_RIGHT_BTTNS[index].setAttribute("style", "display: none;");
+        }
+    }
+
+    // Scrolling implementation
+    let scrollDir = DEFAULT_SCROLL_DIR;
     let scrollStatus = [];
     let scrollFastStatus = [];
     let scrollPositions = [];
     for (let index = 0; index < GALLERIES.length; index++) {
-        // Initializations
-        scrollStatus.push(true);
-        scrollFastStatus.push(false);
-        scrollPositions.push(0);
+        if (!scrollables[index]) {
+            scrollStatus.push(null);
+            scrollFastStatus.push(null);
+            scrollPositions.push(null);
+        } else {
+            // Initializations
+            scrollStatus.push(true);
+            scrollFastStatus.push(false);
+            scrollPositions.push(0);
 
+            // Auto scrolling
+            setInterval(function () {
+                if (scrollStatus[index]) {
+                    GALLERIES_CONTENTS[index].scroll(scrollPositions[index], 0);
+                    scrollPositions[index] += scrollDir;
+                }
+            }, 1 / DEFAULT_SCROLL_SPEED);
 
-        // Auto-scrolling
-        setInterval(function () {
-            if (scrollStatus[index]) {
-                GALLERIES_CONTENTS[index].scroll(scrollPositions[index], 0);
-                scrollPositions[index] += scrollDir;
-            }
-        }, 1 / DEFAULT_SCROLL_SPEED);
+            // Stop scrolling on mouseover and touch events
+            GALLERIES[index].addEventListener("mouseenter", function () {
+                scrollStatus[index] = false;
+            });
 
+            GALLERIES[index].addEventListener("touchstart", function () {
+                scrollStatus[index] = false;
+            });
 
-        // Stop scrolling on mouseover and touch events
-        GALLERIES[index].addEventListener("mouseenter", function () {
-            scrollStatus[index] = false;
-        });
+            GALLERIES[index].addEventListener("mouseleave", function () {
+                scrollStatus[index] = true;
+            });
 
-        GALLERIES[index].addEventListener("touchstart", function () {
-            scrollStatus[index] = false;
-        });
+            GALLERIES[index].addEventListener("touchend", function () {
+                scrollStatus[index] = false;
+            });
 
-        GALLERIES[index].addEventListener("mouseleave", function () {
-            scrollStatus[index] = true;
-        });
+            // Gallery buttons behaviors
+            setInterval(function () {
+                if (scrollFastStatus[index] && scrollPositions[index] >= 0) {
+                    GALLERIES_CONTENTS[index].scroll(scrollPositions[index], 0);
+                    scrollPositions[index] += scrollDir;
+                }
 
-        GALLERIES[index].addEventListener("touchend", function () {
-            scrollStatus[index] = false;
-        });
+                if (scrollPositions[index] < 0) {
+                    scrollPositions[index] = 0;
+                }
+            }, 1 / MAX_SCROLL_SPEED);
 
+            GALLERIES_LEFT_BTTNS[index].addEventListener("mousedown", function () {
+                scrollFastStatus[index] = true;
+                scrollDir = -1;
+            });
 
-        // Gallery buttons behavior
-        setInterval(function () {
-            if (scrollFastStatus[index]) {
-                GALLERIES_CONTENTS[index].scroll(scrollPositions[index], 0);
-                scrollPositions[index] += scrollDir;
-            }
-        }, 1 / FAST_SCROLL_SPEED);
+            GALLERIES_LEFT_BTTNS[index].addEventListener("mouseup", function () {
+                scrollFastStatus[index] = false;
+                scrollDir = DEFAULT_SCROLL_DIR;
+            });
 
-        GALLERIES_LEFT_BTTNS[index].addEventListener("mousedown", function () {
-            scrollFastStatus[index] = true;
-            scrollDir = -1;
-        });
+            GALLERIES_RIGHT_BTTNS[index].addEventListener("mousedown", function () {
+                scrollFastStatus[index] = true;
+                scrollDir = 1;
+            });
 
-        GALLERIES_LEFT_BTTNS[index].addEventListener("mouseup", function () {
-            scrollFastStatus[index] = false;
-            scrollDir = DEFAULT_SCROLL_DIR;
-        });
+            GALLERIES_RIGHT_BTTNS[index].addEventListener("mouseup", function () {
+                scrollFastStatus[index] = false;
+                scrollDir = DEFAULT_SCROLL_DIR;
+            });
 
-        GALLERIES_RIGHT_BTTNS[index].addEventListener("mousedown", function () {
-            scrollFastStatus[index] = true;
-            scrollDir = 1;
-        });
+            // Infinite scrolling
+            setInterval(function () {
+                if (scrollPositions[index] < 0) {
+                    scrollPositions[index] = GALLERIES_CONTENTS[index].scrollWidth / 2;
+                }
 
-        GALLERIES_RIGHT_BTTNS[index].addEventListener("mouseup", function () {
-            scrollFastStatus[index] = false;
-            scrollDir = DEFAULT_SCROLL_DIR;
-        });
+                if (scrollPositions[index] > GALLERIES_CONTENTS[index].scrollWidth / 2) {
+                    scrollPositions[index] = 0;
+                }
+            }, 1 / MAX_SCROLL_SPEED);
+        }
     }
-} else {
-    console.log("ERROR: Card gallery elements does not match each other");
 }
-
-
-/**
- * Gallery infinite scrolling
- */
