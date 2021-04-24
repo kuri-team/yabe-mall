5d4d6b93b3a4bad89f48d1f6d41abc0cf89b3231
miketvo <ikxplicitie@gmail.com>
2021-04-24 09:21:07 +0700

    Implemented store-owner only fields toggle via js

---
+++ public/js/account/register.js
@@ -0,0 +1,11 @@
+const STORE_OWNER_ONLY_FIELDS = document.getElementById("store-owner-only");
+const RADIO_STORE_OWNER = document.getElementById("store-owner");
+const CAPTURE_AREA = document.getElementById("register-account-type-capture-area");
+
+CAPTURE_AREA.addEventListener("click", function () {
+    if (RADIO_STORE_OWNER.checked) {
+        STORE_OWNER_ONLY_FIELDS.setAttribute("style", "display: initial; animation: expand-top 0.5s; transform-origin: top;");
+    } else {
+        STORE_OWNER_ONLY_FIELDS.setAttribute("style", "display: none;");
+    }
+});
