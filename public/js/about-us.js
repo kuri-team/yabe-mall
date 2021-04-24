f714aeb6bc254222089591d4543e4ad1877d38bb
doanyennhi <s3880599@rmit.edu.vn>
2021-04-24 13:57:13 +0700

    Refined modal box function to incorporate closing the modal box

--- public/js/about-us.js
+++ public/js/about-us.js
@@ -1,9 +1,15 @@
-function displayModalBox(modalBoxId) {
+function toggleModalBox(modalBoxId) {
     const PAGEDIM = document.getElementById("dimmed-page");
     const OVERLAY = document.getElementById("overlay-modal-window");
     let modalBox = document.getElementById(modalBoxId);
 
+    if (PAGEDIM.style.display === "none") {
     PAGEDIM.style.display = "block";
     OVERLAY.style.display = "flex";
     modalBox.style.display = "block";
+    } else {
+        PAGEDIM.style.display = "none";
+        OVERLAY.style.display = "none";
+        modalBox.style.display = "none";
+    }
 }
