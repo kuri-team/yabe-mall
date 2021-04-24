eff5f7408574cb1f60a720f63a32e6c6e42f83e8
miketvo <ikxplicitie@gmail.com>
2021-04-24 21:56:21 +0700

    Finished avatar upload fields

--- public/js/account/register.js
+++ public/js/account/register.js
@@ -1,3 +1,18 @@
+// Display file upload field when user clicks on #register-avatar img
+const AVATAR_AREA = document.querySelector("#register-avatar img");
+const UPLOAD_FIELD = document.getElementById("register-avatar-upload-field");
+const DISPLAY_ATTRIBUTE = "display: flex; animation: expand-top 0.45s; transform-origin: top;";
+
+AVATAR_AREA.addEventListener("click", function () {
+    if (UPLOAD_FIELD.getAttribute("style") === DISPLAY_ATTRIBUTE) {
+        UPLOAD_FIELD.setAttribute("style", "display: none");
+    } else {
+        UPLOAD_FIELD.setAttribute("style", DISPLAY_ATTRIBUTE);
+    }
+});
+
+
+// Display store owner only fields when appropriate radio box is checked
 const STORE_OWNER_ONLY_FIELDS = document.getElementById("store-owner-only");
 const RADIO_STORE_OWNER = document.getElementById("store-owner");
 const CAPTURE_AREA = document.getElementById("register-account-type-capture-area");
