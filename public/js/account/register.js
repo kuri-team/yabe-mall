53924a52d6601e833e03f31d456690fa6a7b81fd
miketvo <ikxplicitie@gmail.com>
2021-04-25 09:07:19 +0700

    Added edit icon for avatar

--- public/js/account/register.js
+++ public/js/account/register.js
@@ -1,13 +1,18 @@
 // Display file upload field when user clicks on #register-avatar img
 const AVATAR_AREA = document.querySelector("#register-avatar img");
+const EDIT_ICON = document.querySelector("#register-avatar .edit-icon");
 const UPLOAD_FIELD = document.getElementById("register-avatar-upload-field");
 const DISPLAY_ATTRIBUTE = "display: flex; animation: expand-top 0.45s; transform-origin: top;";
 
 AVATAR_AREA.addEventListener("click", function () {
     if (UPLOAD_FIELD.getAttribute("style") === DISPLAY_ATTRIBUTE) {
         UPLOAD_FIELD.setAttribute("style", "display: none");
+        EDIT_ICON.setAttribute("style", "");
+        EDIT_ICON.lastChild.textContent = " Edit";
     } else {
         UPLOAD_FIELD.setAttribute("style", DISPLAY_ATTRIBUTE);
+        EDIT_ICON.setAttribute("style", "display: block;");
+        EDIT_ICON.lastChild.textContent = " Cancel";
     }
 });
 
