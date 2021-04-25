479030cd431c3904d8aa1dd37df5d4c5a12190ab
miketvo <ikxplicitie@gmail.com>
2021-04-25 13:37:59 +0700

    Reformatting code structure and comments

--- public/js/contact/contact.js
+++ public/js/contact/contact.js
@@ -1,18 +1,18 @@
-// Selector pass value to a function
-function validation(selector) {
+const FORM = document.getElementById("contact-form");
 
+FORM.addEventListener("submit", function () {
     //Get the value of name(selecter) and put in firstName(variable)
-    let firstName = selector.fname.value;
-    let lastName = selector.lname.value;
+    let firstName = FORM.fname.value;
+    let lastName = FORM.lname.value;
 
     //Get the value of phone(selecter) and put in phone_num(Variable)
-    let gPhone = selector.phone_num.value;
+    let gPhone = FORM.phone_num.value;
 
     //Get the value of email(selecter) and put in gEmail(Variable)
-    let gEmail = selector.email_add.value;
+    let gEmail = FORM.email_add.value;
 
     //Get the Value of message(selecter) and put in gMessage (Variable)
-    let gMessage = selector.message.value;
+    let gMessage = FORM.message.value;
 
     //Create RegExp pattern to validate our email address value coming from a form field.
     let emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -68,22 +68,22 @@ function validation(selector) {
     }
 
     /* If you don't  checked 0 index of gender field  or 1 index than show error(Contact Method)*/
-    if (!selector.contact_method[0].checked && !selector.contact_method[1].checked) {
+    if (!FORM.contact_method[0].checked && !FORM.contact_method[1].checked) {
         errNum++;
         err += errNum + ". Select Preferred Contact Method.\n";
     }
 
     /*  If you don't checked any contact day then show error(contact-day-checkbox) */
-    if (!selector.mon.checked && !selector.tues.checked &&
-        !selector.wed.checked && !selector.thur.checked &&
-        !selector.fri.checked && !selector.sat.checked &&
-        !aselectorrg.sun.checked) {
+    if (!FORM.mon.checked && !FORM.tues.checked &&
+        !FORM.wed.checked && !FORM.thur.checked &&
+        !FORM.fri.checked && !FORM.sat.checked &&
+        !aelementrg.sun.checked) {
         errNum++;
         err += errNum + ". Select at least 1 Contact Day.\n";
     }
 
     /*  Check your selection index if your index is less than 1 than show error (Contact Purpose) */
-    if (selector.contact_purpose.selectedIndex < 1) {
+    if (FORM.contact_purpose.selectedIndex < 1) {
         errNum++;
         err += errNum + ". PLease select your contact Purpose.\n";
     }
@@ -98,59 +98,54 @@ function validation(selector) {
         alert('done');
         return true;
     }
+});
+
+
+// At least one checkbox must be checked
+const CHECKBOXES = document.querySelector('#checkbox-form').querySelectorAll('input[type=checkbox]');
+const CHECK_BOX_LENGTH = CHECKBOXES.length;
+const FIRST_CHECK_BOX = CHECK_BOX_LENGTH > 0 ? CHECKBOXES[0] : null;
+
+function isChecked() {
+    for (let i = 0; i < CHECK_BOX_LENGTH; i++) {
+        if (CHECKBOXES[i].checked) return true;
+    }
+    return false;
+}
+
+function checkValidity() {
+    const ERROR_MESSAGE = !isChecked() ? 'Should select at least a box' : '';
+    FIRST_CHECK_BOX.setCustomValidity(ERROR_MESSAGE);
+}
+
+if (FIRST_CHECK_BOX) {
+    for (let i = 0; i < CHECK_BOX_LENGTH; i++) {
+        CHECKBOXES[i].addEventListener('change', checkValidity);
+    }
+
+    checkValidity();
 }
 
 
-// checked boxes must be selected at least 1 option
-(function() {
-    const FORM = document.querySelector('#checkbox-form');
-    const CHECKBOXES = FORM.querySelectorAll('input[type=checkbox]');
-    const CHECKBOXLENGTH = CHECKBOXES.length;
-    const FIRSTCHECKBOX = CHECKBOXLENGTH > 0 ? CHECKBOXES[0] : null;
 
-    function init() {
-        if (FIRSTCHECKBOX) {
-            for (let i = 0; i < CHECKBOXLENGTH; i++) {
-                CHECKBOXES[i].addEventListener('change', checkValidity);
-            }
+// Word-count Messages
+document.getElementById("message").addEventListener("keyup", updateRequirementMessage);
 
-            checkValidity();
-        }
-    }
-
-    function isChecked() {
-        for (let i = 0; i < CHECKBOXLENGTH; i++) {
-            if (CHECKBOXES[i].checked) return true;
-        }
-
-        return false;
-    }
-
-    function checkValidity() {
-        const ERRORMESSAGE = !isChecked() ? 'Should select at least a box' : '';
-        FIRSTCHECKBOX.setCustomValidity(ERRORMESSAGE);
-    }
-
-    init();
-})();
-
-
-// message style to help visitors recognize the error easily
-document.getElementById("message").addEventListener("keyup", myFunction);
-function myFunction() {
+function updateRequirementMessage() {
     let message = document.getElementById("message").value;
     let T = message.length;
-    let advice;
-    if (T < 50){
-        advice = (50-T) + " more letters are needed !!";
+    let advice = "";
+
+    if (T < 50) {
+        advice = "Your message needs " + (50-T) + " more letters.";
         document.getElementById('remaining-letters').innerHTML = advice.fontcolor("red");
     }
     else if (T <= 500) {
-        advice = "You can type " + (500 - T) + " more letters";
-        document.getElementById('remaining-letters').innerHTML = advice.fontcolor("blue");
+        advice = "You have " + (500 - T) + " letters left.";
+        document.getElementById('remaining-letters').innerHTML = advice.fontcolor("green");
     }
     else {
-        advice = "Deleting " + (T - 500) + " letters is needed!!";
+        advice = "Your message is exceeding " + (T - 500) + " letters.";
         document.getElementById('remaining-letters').innerHTML = advice.fontcolor("red");
     }
 }
