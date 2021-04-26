c1bfbcd20be3e7cdd24f085b76c36f407365c17f
doanyennhi <s3880599@rmit.edu.vn>
2021-04-27 01:12:28 +0700

    Fixed bug: wrong pwd message not displayed, form still sends even when pwd is wrong

--- public/js/account/login.js
+++ public/js/account/login.js
@@ -3,13 +3,14 @@ const LOGIN_FORM = document.getElementById("login-form");
 
 // Check entered password with correct password
 LOGIN_FORM.addEventListener("submit", function () {
-    // Get password, wrong password message values, wrong pwd message styles
+    // Get password value and wrong password message
     const ENTERED_PWD = LOGIN_FORM.password.value;
     const WRONG_PWD_MSG = document.getElementById("wrong-pwd");
-    const MSG_STYLE = window.getComputedStyle(WRONG_PWD_MSG);
 
     if (ENTERED_PWD !== CORRECT_PWD) {
-        MSG_STYLE.display = "block";
+        // Display wrong password message
+        WRONG_PWD_MSG.setAttribute("style", "display: block");
+        event.preventDefault();   // Stop form from submitting
         return false;
     } else {
         return true;
