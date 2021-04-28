58838e10d08fa6fda21de2ec9d4cd513d30ace56
rider3458 <s3878480@rmit.edu.vn>
2021-04-28 19:30:48 +0700

    add product's info to local storage with key = "cartProducts" delete sample html div in cart <li> for each product is added from cart-display.js

--- public/js/cart/cart.js
+++ public/js/cart/cart.js
@@ -1,13 +1,42 @@
-// check if cart has any product
-const VIEW_CART_ITEMS = document.querySelector(".cart-body")
-const EMPTY_MESSAGE = document.querySelector(".empty-cart")
-
-if (localStorage.length>0){
-    VIEW_CART_ITEMS.classList.add("active")
-    EMPTY_MESSAGE.classList.add("hide")
+let productID = window.location.pathname.split('/')[5]
+let productName = document.querySelector(".product-info-main h2").innerHTML
+let productLink = window.location.href
+let storeName = document.querySelector(".product-info-main p").innerHTML
+let finalPrice = parseFloat(document.querySelector(".product-info-price").innerHTML.slice(1,))
+let productImg = document.querySelector(".product-info-main-img img").src
+let cartItem = {
+    "product_name": productName,
+    "product_Link": productLink,
+    "store_name": storeName,
+    "final_price": finalPrice,
+    "product_img": productImg,
+    "product_quantity": 0
 }
-else {
-    VIEW_CART_ITEMS.classList.remove("active")
-    EMPTY_MESSAGE.classList.remove("hide")
-}
 
+const BUY_NOW_BUTTON = document.querySelector(".buy-now")
+const ADD_BUTTON = document.querySelector(".add-to-cart")
+ADD_BUTTON.addEventListener("click", addItemToCart)
+BUY_NOW_BUTTON.addEventListener("click", addItemToCart)
+
+function addItemToCart() {
+    let cartProducts = localStorage.getItem("cartProducts");
+    cartProducts = JSON.parse(cartProducts);
+
+    if(cartProducts != null) {
+        if(cartProducts[productID] === undefined) {
+            cartProducts = {
+                ...cartProducts,
+                [productID]: cartItem
+            }
+        }
+        cartProducts[productID].product_quantity += 1
+    }
+    else {
+        cartItem.product_quantity= 1
+        cartProducts = {
+            [productID]: cartItem
+        }
+    }
+
+    localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
+}
