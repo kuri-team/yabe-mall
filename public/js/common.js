7f52a7b81569b05c3c58f6949c319bff40c6dc6f
miketvo <ikxplicitie@gmail.com>
2021-04-30 00:38:36 +0700

    dev-hotfix-2-mobile-menu

--- public/js/common.js
+++ public/js/common.js
@@ -23,6 +23,12 @@ function toggleMobileMenu() {
     }
 }
 
+document.querySelectorAll(".mobile-menu li").forEach(function (element) {
+    element.addEventListener("click", function () {
+        window.location.href = element.querySelector("a").href;
+    });
+});
+
 function scrollToTop() {
     document.body.scrollTop = 0;  // For Safari
     document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
