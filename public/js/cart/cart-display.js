cec4218fdbeb5565ab343d35cea218fbbd66325e
rider3458 <s3878480@rmit.edu.vn>
2021-05-02 20:45:58 +0700

    coupon "COSC2430-HD" and "COSC2430-DI" can added to get 20% and 10% discount respectively applying the wrong coupon code returns invalid msg reload page if remove coupon

--- public/js/cart/cart-display.js
+++ public/js/cart/cart-display.js
@@ -1,5 +1,9 @@
 const VIEW_CART_ITEMS = document.querySelector(".cart-body");
 const EMPTY_MESSAGE = document.querySelector(".empty-cart");
+let discountAmount = 0;
+localStorage.setItem("discountAmount",discountAmount);
+let finalPrice = 0;
+localStorage.setItem("finalPrice",finalPrice);
 
 // clear the localStorage if all items has been removed
 if (localStorage.getItem("cartProducts") === "{}") {
@@ -62,6 +66,13 @@ function  listItemInCart() {
     displayTotalPrice.innerHTML = "$"+totalPrice;
 }
 
+let subTotal = localStorage.getItem("subTotal");
+subTotal = parseFloat(subTotal);
+let totalPrice = localStorage.getItem("totalPrice");
+totalPrice = parseFloat(totalPrice);
+let discountValue = totalPrice * discountAmount;
+localStorage.setItem("discountValue", discountValue);
+
 // change quantity with + - buttons
 // + button: update current quantity, subTotal and totalPrice on the page and local storage
 document.querySelectorAll(".cart-product-quantity-button.plus").forEach(function (item) {
@@ -78,18 +89,30 @@ document.querySelectorAll(".cart-product-quantity-button.plus").forEach(function
         let totalPrice = localStorage.getItem("totalPrice"); // update totalPrice in localStorage
         totalPrice = parseFloat(totalPrice);
         totalPrice += productList[productID].final_price;
+        discountValue = totalPrice * discountAmount;
+        finalPrice = totalPrice - discountValue;
         totalPrice = parseFloat(totalPrice.toFixed(2));
+        discountValue = parseFloat(discountValue.toFixed(2));
+        finalPrice = parseFloat(finalPrice.toFixed(2));
         localStorage.setItem("totalPrice",JSON.stringify(totalPrice));
         localStorage.setItem("cartProducts",JSON.stringify(productList));
+        localStorage.setItem("discountValue",JSON.stringify(discountValue));
+        localStorage.setItem("finalPrice",JSON.stringify(finalPrice));
+
 
         let inputValue = document.getElementById(`${ productID } product-quantity`).value; // page
         let productQuantity = parseInt(inputValue)
         productQuantity += 1;
         document.getElementById(`${ productID } product-quantity`).value = productQuantity;
         let displaySubTotal = document.querySelector(".cart-product-total .cart-product-total-fee");
-        let displayTotalPrice = document.querySelector(".cart-product-fees-total .cart-product-total-fee");
-        displaySubTotal.innerHTML = "$"+subTotal;
-        displayTotalPrice.innerHTML = "$"+totalPrice;
+        let displayFinalPrice = document.querySelector(".cart-product-fees-total .cart-product-total-fee");
+        displaySubTotal.innerHTML = "$" + subTotal;
+        displayFinalPrice.innerHTML = "$" + finalPrice;
+
+        if (document.getElementById("insert-coupon").disabled === true) {
+            let displayDiscountValue = document.querySelector(".cart-product-coupon .cart-product-total-fee");
+            displayDiscountValue.innerHTML = "- $" + discountValue;
+        }
 });
 });
 
@@ -110,8 +133,16 @@ document.querySelectorAll(".cart-product-quantity-button.minus").forEach(functio
             subTotal = parseFloat(subTotal.toFixed(2));
             localStorage.setItem("subTotal",JSON.stringify(subTotal));// update subTotal in localStorage
             totalPrice -= productList[productID].final_price;
+            discountValue = totalPrice * discountAmount;
+            finalPrice = totalPrice - discountValue;
             totalPrice = parseFloat(totalPrice.toFixed(2));
-            localStorage.setItem("totalPrice",JSON.stringify(totalPrice)); // update totalPrice in localStorage
+            discountValue = parseFloat(discountValue.toFixed(2));
+            finalPrice = parseFloat(finalPrice.toFixed(2));
+            localStorage.setItem("totalPrice",JSON.stringify(totalPrice));
+            localStorage.setItem("cartProducts",JSON.stringify(productList));
+            localStorage.setItem("discountValue",JSON.stringify(discountValue));
+            localStorage.setItem("finalPrice",JSON.stringify(finalPrice));
+
         }
 
         if ( productList[productID].product_quantity < 1){
@@ -128,9 +159,14 @@ document.querySelectorAll(".cart-product-quantity-button.minus").forEach(functio
 
         document.getElementById(`${productID} product-quantity`).value = productQuantity;
         let displaySubTotal = document.querySelector(".cart-product-total .cart-product-total-fee");
-        let displayTotalPrice = document.querySelector(".cart-product-fees-total .cart-product-total-fee");
+        let displayFinalPrice = document.querySelector(".cart-product-fees-total .cart-product-total-fee");
         displaySubTotal.innerHTML = "$"+subTotal;
-        displayTotalPrice.innerHTML = "$"+totalPrice;
+        displayFinalPrice.innerHTML = "$"+finalPrice;
+
+        if (document.getElementById("insert-coupon").disabled === true) {
+            let displayDiscountValue = document.querySelector(".cart-product-coupon .cart-product-total-fee");
+            displayDiscountValue.innerHTML = "- $" + discountValue;
+        }
     });
 });
 
@@ -145,3 +181,51 @@ document.querySelectorAll(".cart-product-remove-button").forEach(function (item)
         location.reload();
     })
 })
+
+// apply coupon
+const APPLY_COUPON = document.querySelector(".apply-coupon-button");
+let displayFinalPrice = document.querySelector(".cart-product-fees-total .cart-product-total-fee");
+let removeAfterApply = document.querySelector(".apply-coupon-button");
+let applyAfterRemove = document.querySelector(".remove-coupon-button");
+let couponMsg = document.querySelector(".coupon-msg");
+
+APPLY_COUPON.addEventListener("click", function() {
+    let checkCoupon = document.getElementById("insert-coupon").value;
+    if (checkCoupon === "COSC2430-HD") {
+        discountAmount = 0.2;
+        applyDiscount();
+        removeCoupon();
+    } else if (checkCoupon === "COSC2430-DI") {
+        discountAmount = 0.1;
+        applyDiscount();
+        removeCoupon();
+    } else {
+        couponMsg.classList.add("invalid");
+        couponMsg.innerHTML = "Invalid coupon code";
+    }
+})
+
+// apply discount and add discount value on page
+function applyDiscount () {
+    discountValue = totalPrice * discountAmount;
+    localStorage.setItem("discountAmount",JSON.stringify(discountAmount));
+    finalPrice = totalPrice - discountValue;
+    discountValue = parseFloat(discountValue.toFixed(2));
+    finalPrice = parseFloat(finalPrice.toFixed(2));
+    localStorage.setItem("discountValue",discountValue);
+    localStorage.setItem("finalPrice", JSON.stringify(finalPrice));
+    displayFinalPrice.innerHTML = "$" + finalPrice;
+    document.querySelector(".cart-product-coupon .cart-product-total-fee").innerHTML = "- $"+discountValue;
+    removeAfterApply.classList.add("hide");
+    applyAfterRemove.classList.add("active");
+    couponMsg.classList.remove("invalid");
+    couponMsg.innerHTML = "Coupon applied";
+    document.getElementById("insert-coupon").disabled = true;
+}
+
+// reload after remove coupon
+function removeCoupon () {
+    applyAfterRemove.addEventListener("click", function() {
+        location.reload();
+    })
+}
