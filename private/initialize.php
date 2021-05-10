b65ac80f76c3ffee811740384377a380000d62dc
miketvo <ikxplicitie@gmail.com>
2021-05-11 03:08:03 +0700

    Reindent common php codes; Created install.php

--- private/initialize.php
+++ private/initialize.php
@@ -1,24 +1,24 @@
 <?php
-
-  ob_start(); // output buffering is turned on
-
-  // Assign file paths to PHP constants
-  // __FILE__ returns the current path to this file
-  // dirname() returns the path to the parent directory
-  define("PRIVATE_PATH", dirname(__FILE__));
-  define("PROJECT_PATH", dirname(PRIVATE_PATH));
-  define("PUBLIC_PATH", PROJECT_PATH . '/public');
-  define("SHARED_PATH", PRIVATE_PATH . '/shared');
-
-  // Assign the root URL to a PHP constant
-  // * Do not need to include the domain
-  // * Use same document root as webserver
-  // * Can set a hardcoded value:
-  // define("WWW_ROOT", '/public');
-  // define("WWW_ROOT", '');
-  // * Can dynamically find everything in URL up to "/public"
-  define("WWW_ROOT", "");
-
-  require_once('functions.php');
-
-  $errors = [];
+    
+    ob_start(); // output buffering is turned on
+    
+    // Assign file paths to PHP constants
+    // __FILE__ returns the current path to this file
+    // dirname() returns the path to the parent directory
+    define("PRIVATE_PATH", dirname(__FILE__));
+    define("PROJECT_PATH", dirname(PRIVATE_PATH));
+    define("PUBLIC_PATH", PROJECT_PATH . '/public');
+    define("SHARED_PATH", PRIVATE_PATH . '/shared');
+    
+    // Assign the root URL to a PHP constant
+    // * Do not need to include the domain
+    // * Use same document root as webserver
+    // * Can set a hardcoded value:
+    // define("WWW_ROOT", '/public');
+    // define("WWW_ROOT", '');
+    // * Can dynamically find everything in URL up to "/public"
+    define("WWW_ROOT", "");
+    
+    require_once('functions.php');
+    
+    $errors = [];
