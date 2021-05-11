993a6fe2775a47c1faa1e4c0de1a1fbc82fd7d54
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-11 11:42:37 +0700

    Change html files into PHP in the following folders + change top & bottom elements of legal/tos page

---
+++ public/mall/legal/index.php
@@ -0,0 +1,25 @@
+<?php require_once('../../../../private/initialize.php'); ?>
+
+<?php
+
+$page_title = 'Copyright';
+$style_sheets = [
+    "/css/common.css",
+    "/css/legal/legal.css",
+];
+$scripts = [
+    "/js/common.js",
+];
+
+include(SHARED_PATH . '/top.php');
+
+?>
+
+  <h1>To be further implemented</h1>
+  <ul>
+    <li><a href="tos">Terms of Service</a></li>
+    <li><a href="privacy-policy">Privacy Policy</a></li>
+    <li><a href="copyright">Copyright</a></li>
+  </ul>
+
+<?php include(SHARED_PATH . '/bottom.php'); ?>
