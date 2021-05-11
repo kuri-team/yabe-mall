404bbba4b314b04de390656c4ba6302cf011e8a0
doanyennhi <s3880599@rmit.edu.vn>
2021-05-11 18:36:58 +0700

    Changed file extension, top & bottom elements of Support

---
+++ public/mall/support/index.php
@@ -0,0 +1,24 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php
+
+    $page_title = 'Yabe | Support';
+    $style_sheets = [
+        "/css/common.css",
+        "/css/legal/legal.css",
+    ];
+    $scripts = [
+        "/js/common.js",
+    ];
+
+    include(SHARED_PATH . '/top.php');
+
+?>
+
+<h1>To be further implemented</h1>
+<ul>
+  <li><a href="faq">Frequently Asked Questions</a></li>
+  <li><a href="pricing">Pricing</a></li>
+</ul>
+
+<?php include(SHARED_PATH . '/bottom.php'); ?>
