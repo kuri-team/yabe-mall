a3bb42d0b81f8312d08670953b4fc13bb2e5887b
doanyennhi <s3880599@rmit.edu.vn>
2021-05-12 21:42:58 +0700

    Fixed links in Mall Contact page

--- public/mall/contact/index.php
+++ public/mall/contact/index.php
@@ -18,8 +18,8 @@ include(SHARED_PATH . "/top.php");
 
 <main>
   <ul class="breadcrumb">
-    <li><a href="../">Home</a>/</li>
-    <li><a href="#">Contact Us</a></li>
+    <li><a href="<?=url_for("/mall");?>">Home</a>/</li>
+    <li><a href="<?=url_for("/mall/contact");?>">Contact Us</a></li>
   </ul>
   <h1 class="content-title">CONTACT US</h1>
 
