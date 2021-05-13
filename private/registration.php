54f2e11387a31e58fc11a8ba5a62026c3f78ff60
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-13 13:37:46 +0700

    converts special characters to HTML entities sends the submitted form data to the page itself

--- private/registration.php
+++ private/registration.php
@@ -7,35 +7,38 @@
         return boolval(preg_match($email_regex,$email));
     }
 
-    function validate_length($value, $minLength): bool {
+    function validate_length($value, $minLength): bool
+    {
         $valueStrLen = strlen($value);
 
-        if ($valueStrLen < $minLength) {
+        if ($valueStrLen < $minLength): {
             return boolval($value);
+        }
+
+
+            function register_validation($data)
+            {
+                $data = trim($data);
+                $data = htmlspecialchars($data);
+                return $data;
             }
-}
 
+            function validate_tel($tel): bool
+            {
+                $phone_regex = "/^([0-9][-. ]?){8,10}[0-9]$/";
+                return boolval(preg_match($phone_regex, $tel));
+            }
 
+            function validate_zipcode($zipcode): bool
+            {
+                // sanitize user input in zipcode
+                $zipcode = filter_var($zipcode, FILTER_SANITIZE_NUMBER_INT);
+                $zipcode_regex = "/^[0-9]{4,6}$/";
+                return boolval(preg_match($zipcode_regex, $zipcode));
+            }
 
-function register_validation($data) {
-    $data = trim($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-    function validate_tel($tel): bool {
-        $phone_regex = "/^([0-9][-. ]?){8,10}[0-9]$/";
-        return boolval(preg_match($phone_regex, $tel));
-    }
-
-    function validate_zipcode($zipcode): bool {
-        // sanitize user input in zipcode
-        $zipcode = filter_var($zipcode, FILTER_SANITIZE_NUMBER_INT);
-        $zipcode_regex = "/^[0-9]{4,6}$/";
-        return boolval(preg_match($zipcode_regex, $zipcode));
-    }
-    
-    function validate_pwd($pwd): bool {
-        $pwd_regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/";
-        return boolval(preg_match($pwd_regex, $pwd));
-    }
+            function validate_pwd($pwd): bool
+            {
+                $pwd_regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/";
+                return boolval(preg_match($pwd_regex, $pwd));
+            }
