fb809d2293abb9a4ae7335f53ca6e0b553b91359
doanyennhi <s3880599@rmit.edu.vn>
2021-05-13 12:50:59 +0700

    Implemented validate function for phone number and zipcode

--- private/registration.php
+++ private/registration.php
@@ -1,5 +1,13 @@
 <?php
     
-    function validate_phone($phone) {
+    function validate_tel($tel): bool {
+        $phone_regex = "/^([0-9][-. ]?){8,10}[0-9]$/";
+        return boolval(preg_match($phone_regex,$tel));
+    }
     
+    function validate_zipcode($zipcode): bool {
+        // sanitize user input in zipcode
+        $zipcode = filter_var($zipcode, FILTER_SANITIZE_NUMBER_INT);
+        $zipcode_regex = "/^[0-9]{4,6}$/";
+        return boolval(preg_match($zipcode_regex,$zipcode));
     }
