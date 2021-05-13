9e2de31c43e597c0c33e63f524b5647d3845d51d
miketvo <ikxplicitie@gmail.com>
2021-05-13 22:21:39 +0700

    Removed all javascript redirections

--- public/js/account/my-account.js
+++ public/js/account/my-account.js
@@ -6,10 +6,3 @@ const LOGIN_EMAIL = document.getElementById("login-email");
 
 // Change email field to entered email value
 LOGIN_EMAIL.innerHTML = USER_EMAIL;
-
-// Redirect the user back to Login page if the user hasn't logged in
-if (localStorage.isLoggedIn === null || localStorage.isLoggedIn === "false") {
-    let url = window.location.href;
-    url = url.replace("my-account", "login");
-    window.location.replace(url);
-}
