abba871770aaff9015aae0238d53cbeead8fad21
miketvo <ikxplicitie@gmail.com>
2021-05-14 23:49:06 +0700

    Updated get_country_name()

--- private/functions.php
+++ private/functions.php
@@ -1,6 +1,6 @@
 <?php
     
-    function url_for($script_path): string {
+    function url_for(string $script_path): string {
         // add the leading "/" if not present
         if($script_path[0] != "/") {
             $script_path = "/" . $script_path;
@@ -8,9 +8,19 @@
         return WWW_ROOT . $script_path . SID;
     }
     
-    function redirect_to($location) {
+    function redirect_to(string $location) {
         header("Location: " . $location);
         exit;
+    }
+    
+    function get_country_name(string $country_code): string {
+        $countries = read_csv(PRIVATE_PATH . "database/country.csv", true);
+        foreach ($countries as $country) {
+            if ($country["country_code"] === $country_code) {
+                return $country["country_name"];
+            }
+        }
+        return "Invalid country code";
     }
     
     function display_errors($errors=array()): string {
