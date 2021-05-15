8c29bb34247808a5445baa661aba9e8056878240
miketvo <ikxplicitie@gmail.com>
2021-05-16 02:05:49 +0700

    Update legal page layout

--- public/mall/legal/index.php
+++ public/mall/legal/index.php
@@ -14,16 +14,21 @@ $scripts = [
 include(SHARED_PATH . "/top.php");
 
 ?>
-
-  <h1>To be further implemented</h1>
-  <ul>
-    <li><a href="<?=url_for("/mall/legal/tos");?>">Terms of Service</a></li>
-    <li><a href="<?=url_for("/mall/legal/privacy-policy");?>">Privacy Policy</a></li>
-    <li><a href="<?=url_for("/mall/legal/copyright");?>">Copyright</a></li>
-  </ul>
   
   <main>
-  
+    <ul class="breadcrumb">
+      <li><a href="<?=url_for("/mall");?>">Home</a>/</li>
+      <li><a href="<?=url_for("/mall/legal");?>">Legal</a></li>
+    </ul>
+
+    <h1 class="content-title">LEGAL</h1>
+    <div class="content-body">
+      <ul>
+        <li><a href="<?=url_for("/mall/legal/tos");?>">Terms of Service</a></li>
+        <li><a href="<?=url_for("/mall/legal/privacy-policy");?>">Privacy Policy</a></li>
+        <li><a href="<?=url_for("/mall/legal/copyright");?>">Copyright</a></li>
+      </ul>
+    </div>
   </main>
 
 <?php include(SHARED_PATH . "/bottom.php"); ?>
