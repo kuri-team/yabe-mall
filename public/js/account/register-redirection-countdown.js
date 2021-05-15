3a5107bad82cdc61bfb464f732ac1aead9b85c1d
miketvo <ikxplicitie@gmail.com>
2021-05-15 18:26:23 +0700

    Refine countdown javascript logic

--- public/js/account/register-redirection-countdown.js
+++ public/js/account/register-redirection-countdown.js
@@ -1,11 +1,13 @@
 const COUNTDOWN = document.getElementById("redirect-countdown");
-let countdownSec = 5;
+const COUNTDOWN_DURATION = 5;
+let countdownSec = COUNTDOWN_DURATION;
 
+COUNTDOWN.innerHTML = `${countdownSec}`;
 setInterval(function () {
-    COUNTDOWN.innerHTML = `${countdownSec--}`;
+    COUNTDOWN.innerHTML = `${--countdownSec}`;
 }, 1000);
 
 setTimeout(function () {
     let currentLink = window.location.href;
     window.location.href = currentLink.replace("register", "login");
-}, 5000);
+}, COUNTDOWN_DURATION * 1000);
