2ac81e5bd43c030d2d8a8c19e59fbede3e55bc6b
miketvo <ikxplicitie@gmail.com>
2021-05-18 07:28:54 +0700

    Remove debug code in register-redirection-countdown.js

--- public/js/account/register-redirection-countdown.js
+++ public/js/account/register-redirection-countdown.js
@@ -3,7 +3,6 @@ const COUNTDOWN = document.getElementById("redirect-countdown");
 const COUNTDOWN_DURATION = 5;
 let countdownSec = COUNTDOWN_DURATION;
 
-console.log(LOGIN_URL);
 
 COUNTDOWN.innerHTML = `${countdownSec}`;
 setInterval(function () {
