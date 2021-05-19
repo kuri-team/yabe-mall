83461338dec1b0b8f68fa58bd52b50f19830b01b
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-19 11:41:20 +0700

    added documentation for checking featured products on home and mall page

--- private/check-featured-stores-products.php
+++ private/check-featured-stores-products.php
@@ -2,6 +2,14 @@
     require_once "csv.php";
     require_once "../private/database/products.csv";
     
+    /**
+     * Check if products are featured on the Mall Home page
+     * user input
+     * @param int $product_id
+     * id required for the input
+     * <strong><em>true</em></strong> if user input has TRUE value,
+     * <strong><em>false</em></strong> otherwise.
+     */
     function check_featured_products($product_id) {
         $products = read_csv("../../../../private/database/products.csv", true);
         foreach ($products as $p) {
@@ -12,6 +20,14 @@
         return false;
     }
     
+    /**
+     * Check if Check if featured products on the Store Home page(s)
+     * user input
+     * @param int $store_id
+     * id required for the input
+     * <strong><em>true</em></strong> if user input has TRUE value,
+     * <strong><em>false</em></strong> otherwise.
+     */
     function check_featured_stores($store_id) {
         $stores = read_csv("../../../../private/database/products.csv", true);
         foreach ($stores as $s) {
