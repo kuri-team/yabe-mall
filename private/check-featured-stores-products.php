2d95c514a097e35ea5b69efb7b7f1a4ada89c0d9
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-19 11:34:27 +0700

    debugged functions to check featured products and stores

--- private/check-featured-stores-products.php
+++ private/check-featured-stores-products.php
@@ -1,10 +1,11 @@
 <?php
     require_once "csv.php";
+    require_once "../private/database/products.csv";
     
     function check_featured_products($product_id) {
         $products = read_csv("../../../../private/database/products.csv", true);
         foreach ($products as $p) {
-            if (($p['featured_in_mall'] === TRUE) && ($p['id'] == $product_id)) {
+            if (($p['featured_in_mall'] == TRUE) && ($p['id'] == $product_id)) {
                 return $p;
             }
         }
@@ -14,7 +15,7 @@
     function check_featured_stores($store_id) {
         $stores = read_csv("../../../../private/database/products.csv", true);
         foreach ($stores as $s) {
-            if (($s['featured_in_store'] === TRUE) && ($s['id'] == $store_id)) {
+            if (($s['featured_in_store'] == TRUE) && ($s['id'] == $store_id)) {
                 return $s;
             }
         }
