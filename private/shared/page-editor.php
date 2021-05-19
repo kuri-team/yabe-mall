c3b61aebb090e3290bc844d9aaa343e83899e309
miketvo <ikxplicitie@gmail.com>
2021-05-19 22:44:18 +0700

    Added a shared page-editor.php module in private/shared

---
+++ private/shared/page-editor.php
@@ -0,0 +1,6 @@
+<div class="page-editor">
+  <form action="" method="post" target="_self">
+    <label><textarea id="page-editor-textarea"></textarea></label>
+    <div id="page-editor-preview"></div>
+  </form>
+</div>
