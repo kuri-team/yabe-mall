8d9406de396a134445181e42ac64e5cc75e32c16
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-19 11:26:06 +0700

    added functions to check featured products and stores

--- private/check-featured-stores-products.php
+++ private/check-featured-stores-products.php
@@ -1,1 +1,24 @@
 <?php
+    require_once "csv.php";
+    
+    function check_featured_products($product_id) {
+        $products = read_csv("../../../../private/database/products.csv", true);
+        foreach ($products as $p) {
+            if (($p['featured_in_mall'] === TRUE) && ($p['id'] == $product_id)) {
+                return $p;
+            }
+        }
+        return false;
+    }
+    
+    function check_featured_stores($store_id) {
+        $stores = read_csv("../../../../private/database/products.csv", true);
+        foreach ($stores as $s) {
+            if (($s['featured_in_store'] === TRUE) && ($s['id'] == $store_id)) {
+                return $s;
+            }
+        }
+        return false;
+    }
+    
+    
