5a657c2f4e2b6629d9be6e368cce46fce106e0c5
doanyennhi <s3880599@rmit.edu.vn>
2021-05-20 00:39:48 +0700

    Renamed file to dynamic-display.php, moved sort_by_time function to this file

--- private/dynamic-display.php
+++ private/dynamic-display.php
@@ -1,4 +1,6 @@
 <?php
+    
+    
     /**
      * Check if products are featured on the Mall Home page
      * @param array $products containing information of all products
@@ -16,6 +18,7 @@
         return false;
     }
     
+    
     /**
      * Check if products are featured on Store Home page(s)
      * @param array $products containing information of all products
@@ -31,5 +34,16 @@
             }
         }
         return false;
+    }
+    
+    
+    /**
+     * Compare and sort the dates created of two items (stores, products, etc.) from newest to oldest
+     * @param array $item1 first item for comparison
+     * @param array $item2 second item for comparison
+     * @return int
+     */
+    function sort_by_time(array $item1, array $item2): int {
+        return -(strtotime($item1["created_time"]) - strtotime($item2["created_time"]));
     }
     
