769ee86eacfbb5c0ef4370ce42462c0024644f6d
doanyennhi <s3880599@rmit.edu.vn>
2021-05-19 23:33:06 +0700

    Removed unnecessary "require" line, added quotes

--- private/check-featured-stores-products.php
+++ private/check-featured-stores-products.php
@@ -1,6 +1,4 @@
 <?php
-    require_once "csv.php";
-    
     /**
      * Check if products are featured on the Mall Home page
      * @param array $products containing information of all products
@@ -11,7 +9,7 @@
      */
     function check_featured_mall_products(array $products) {
         foreach ($products as $p_mall) {
-            if ($p_mall['featured_in_mall'] === TRUE) {
+            if ($p_mall['featured_in_mall'] === "TRUE") {
                 return $p_mall;
             }
         }
