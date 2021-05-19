dc5a9416d5a999faccb3ea03d1908fda3566846f
miketvo <ikxplicitie@gmail.com>
2021-05-19 14:07:24 +0700

    Moved bio section of the About Us page into about-us-bio.php

--- public/mall/about-us/index.php
+++ public/mall/about-us/index.php
@@ -77,7 +77,9 @@ include(SHARED_PATH . "/top.php");
           <span>|</span>
           <span><a href="https://github.com/kuri-team">Github</a></span>
         </h1>
-        <p>Kuri Team was founded during semester 2020C at RMIT University Vietnam, Saigon South Campus. As a young organization, our goal is to support each other in our projects, academic success, and personal development. Our last study project is a game prototype, proposed by us in our assignment paper for COSC2083 - Introduction to IT, to be implemented into RMIT wi-fi network. More detail on that project and proposal to RMIT can be found <a href="https://miketvo.com/blog/project-idea-nov-2020.html">here</a>. The playable prototype can be accessed <a href="https://miketvo.com/project/rmit-sgs-network-game/game.html">here</a>.</p>
+        <section id="about-us-content-bio">
+          <?php include(SHARED_PATH . "/about-us-bio.php"); ?>
+        </section>
         <p><strong>The Yabe online mall e-commerce website is our latest study project. It involves the following Kuri members:</strong></p>
       </div>
     </div>
