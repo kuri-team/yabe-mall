0cbc130a8c71a62c329a8d800af663e36b9b96b6
doanyennhi <s3880599@rmit.edu.vn>
2021-05-20 00:33:11 +0700

    Added docstring for time sort function in created-time-function.php

--- private/created-time-function.php
+++ private/created-time-function.php
@@ -1,6 +1,15 @@
 <?php
-    function sort_by_time($item1, $item2): int {
+    
+    
+    /**
+     * Compare and sort the dates created of two items (stores, products, etc.) from newest to oldest
+     * @param array $item1 first item for comparison
+     * @param array $item2 second item for comparison
+     * @return int
+     */
+    function sort_by_time(array $item1, array $item2): int {
         return -(strtotime($item1["created_time"]) - strtotime($item2["created_time"]));
     }
+    
     
     
