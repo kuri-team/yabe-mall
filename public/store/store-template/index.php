245d7944972856a1e6e8b73b804561f8913a1177
doanyennhi <s3880599@rmit.edu.vn>
2021-05-21 17:05:11 +0700

    Deleted all product cards, write functions to display product cards, added id to store link for testing

--- public/store/store-template/index.php
+++ public/store/store-template/index.php
@@ -1,4 +1,7 @@
-<?php require_once("../../../private/initialize.php"); ?>
+<?php
+    require_once("../../../private/initialize.php");
+    require_once("../../../private/functions.php");
+?>
 
 <?php
 
@@ -15,6 +18,19 @@
     ];
 
     include(SHARED_PATH . "/top.php");
+    
+    
+    function display_product_cards() {
+        echo "<div class='product-card'>";
+        echo "<a href='" . url_for("/store/store-template/product-detail") . "'><img alt='image of a product'
+                  src='../../media/image/hsy_shop/HSY_main_cover_square.jpg'></a>";
+        echo "<div class='product-card-details'>";
+        echo "<a class='product-card-title' href='" . url_for("/store/store-template/product-detail") . "'>Purple Hyacinth Comic</a>";
+        echo "<p class='product-card-shop'>Sophism &amp; Ephemerys</p>";
+        echo "<p class='product-card-price'>$16.95</p>";
+        echo "<div class='product-card-sale-card'>18/6/2018</div>";
+        echo "</div>" . "\n" . "</div>";
+    }
 
 ?>
 
@@ -30,71 +46,6 @@
 
           <section class="store-product-cards">
             <div class="flex-container flex-justify-content-space-between flex-align-items-center flex-wrap">
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY_main_cover_square.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Purple Hyacinth Comic</a>
-                  <p class="product-card-shop">Sophism &amp; Ephemerys</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">18/6/2018</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail-2");?>"><img alt="image of a product"
-                  src="../../media/image/product-detail-2/yoohankim-trio_square.png"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail-2");?>">Omniscient Reader's Viewpoint Novel</a>
-                  <p class="product-card-shop">Sing Shong</p>
-                  <p class="product-card-price">$13.50</p>
-                  <div class="product-card-sale-card">6/1/2018</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY3.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY4.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY5.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY6.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
             </div>
           </section>
         </section>
@@ -107,71 +58,7 @@
 
           <section class="store-product-cards">
             <div class="flex-container flex-justify-content-space-between flex-align-items-center flex-wrap">
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY7.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY8.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/HSY9.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/dan-gold-zvFrn0Ws7cw-unsplash.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/paolo-chiabrando-9dXSoi6VXEA-unsplash.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
-
-              <div class="product-card">
-                <a href="<?=url_for("/store/store-template/product-detail");?>"><img alt="image of a product"
-                  src="../../media/image/hsy_shop/thought-catalog-V5BGaJ0VaLU-unsplash.jpg"></a>
-                <div class="product-card-details">
-                  <a class="product-card-title" href="<?=url_for("/store/store-template/product-detail");?>">Product Title Goes Here</a>
-                  <p class="product-card-shop">Short Description Goes Here</p>
-                  <p class="product-card-price">$16.95</p>
-                  <div class="product-card-sale-card">1/4/2020</div>
-                </div>
-              </div>
+              
             </div>
           </section>
         </section>
