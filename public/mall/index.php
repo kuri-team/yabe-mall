f92540ad2faa08f5872b325991070d475547f9da
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-22 16:27:52 +0700

    In Featured Products and Featured Stores, fixed id in links to product-detail and store

--- public/mall/index.php
+++ public/mall/index.php
@@ -83,10 +83,10 @@
                     $featured_mall_products = check_featured_mall_products($products);
                     while ($display_count < FEATURED_PRODUCTS_DISPLAY_NUM) {
                         echo "<div class='product-card'>
-                        <a href='" . url_for("/store/store-template/product-detail?id={$products[$display_count]['id']}") . "'><img alt='image of a product' src='../media/image/placeholder_262x250.png'></a>
+                        <a href='" . url_for("/store/store-template/product-detail?id={$featured_mall_products[$display_count]['id']}") . "'><img alt='image of a product' src='../media/image/placeholder_262x250.png'></a>
                         <div class='product-card-details'>
-                          <a class='product-card-title' href='" . url_for("/store/store-template/product-detail?id={$products[$display_count]['id']}") . "'>" . $featured_mall_products[$display_count]["name"] . "</a>
-                          <a class='product-card-shop' href='" . url_for("/store/store-template?id={$products[$display_count]['store_id']}") . "'>" . get_store_name((int) $featured_mall_products[$display_count]["store_id"], $stores) . "</a>
+                          <a class='product-card-title' href='" . url_for("/store/store-template/product-detail?id={$featured_mall_products[$display_count]['id']}") . "'>" . $featured_mall_products[$display_count]["name"] . "</a>
+                          <a class='product-card-shop' href='" . url_for("/store/store-template?id={$featured_mall_products[$display_count]['store_id']}") . "'>" . get_store_name((int) $featured_mall_products[$display_count]["store_id"], $stores) . "</a>
                           <p class='product-card-price'>$" . $featured_mall_products[$display_count]["price"] . "</p>
                           <div class='product-card-sale-card'>" . substr($featured_mall_products[$display_count]["created_time"],0,10) . "</div>
                         </div>
@@ -112,8 +112,8 @@
                     $featured_mall_stores = check_featured_mall_stores($stores);
                     while ($display_count < FEATURED_STORES_DISPLAY_NUM) {
                         echo "<div class='store-card'>
-                        <a href='" . url_for("/store/store-template?id={$stores[$display_count]['id']}") . "'><img class='store-card-thumbnail' alt='image representation of a shop' src='../media/image/profile-placeholder_143x143.png'></a>
-                        <a class='store-card-name' href='" . url_for("/store/store-template?id={$stores[$display_count]['id']}") . "'>" . $featured_mall_stores[$display_count]["name"] . "</a>
+                        <a href='" . url_for("/store/store-template?id={$featured_mall_stores[$display_count]['id']}") . "'><img class='store-card-thumbnail' alt='image representation of a shop' src='../media/image/profile-placeholder_143x143.png'></a>
+                        <a class='store-card-name' href='" . url_for("/store/store-template?id={$featured_mall_stores[$display_count]['id']}") . "'>" . $featured_mall_stores[$display_count]["name"] . "</a>
                       </div>";
                         $display_count++;
                     }
