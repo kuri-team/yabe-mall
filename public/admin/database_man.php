04a964a017a9af420d9a73c4d89843611c8ebd3d
miketvo <ikxplicitie@gmail.com>
2021-05-25 01:20:23 +0700

    First edit of Admin Manual; renamed .dtbm-table-title to .admin-content-section-title

--- public/admin/database_man.php
+++ public/admin/database_man.php
@@ -69,7 +69,7 @@
         <?php
         
             foreach ($database_names as $database_name) {
-                echo "<h2 id='" . $database_name . "' class='dtbm-table-title'>" . $database_name . "<i class='fas fa-low-vision toggle-visibility'></i><a href='#'><i class='fas fa-angle-up ml-10'></i></a></h2>";
+                echo "<h2 id='" . $database_name . "' class='admin-content-section-title'>" . $database_name . "<i class='fas fa-low-vision toggle-visibility'></i><a href='#'><i class='fas fa-angle-up ml-10'></i></a></h2>";
                 $database = read_csv("../../private/database/" . $database_name, true);
                 if (count($database) === 0) {
                     $database = read_csv("../../private/database/" . $database_name);
