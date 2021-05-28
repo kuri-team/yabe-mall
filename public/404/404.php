244b04f901e812a4cfca934e26928a2de3db3145
tnathu-ai <s3879312@rmit.edu.vn>
2021-05-28 15:02:34 +0700

    Increased contrast between messages

--- public/404/404.php
+++ public/404/404.php
@@ -20,7 +20,7 @@
         <img class="far" src="../media/image/yabe_cursor.gif" alt="YABE crying face">
         <div class="second-4">4</div>
         <div
-        <h2 class="message">SORRY, PAGE NOT FOUND.</h2>
+            <h2 class="message">SORRY, PAGE NOT FOUND.</h2>
             <p>Don't cry. We have more interesting features than your crush <a href="<?=url_for("/mall");?>">Here</a>!</p>
         </div>
     </div>
