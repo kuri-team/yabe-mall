1434efa85f109479cebecf65be21232bdb16342f
miketvo <ikxplicitie@gmail.com>
2021-05-30 15:51:16 +0700

    Added debugging code to display headers

--- private/initialize.php
+++ private/initialize.php
@@ -46,6 +46,8 @@
     spl_autoload_register("yabe_autoload");
     require_once("functions.php");
     
+    display_headers($_SERVER["REQUEST_URI"]);
+    
     $errors = [];
     
     // install.php logic
